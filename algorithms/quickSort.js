7153f461cfc39961fefeac3f19c9eee71671cac6
stone-yang <stone-yang@hotmail.com>
2017-08-05 13:03:31 +0800

    edit algorithm quick sort

--- algorithms/quickSort.js
+++ algorithms/quickSort.js
@@ -1,38 +1,42 @@
 /**
- * 快速排序
+ * 快速排序(在原数组上排序)
  * 平均时间复杂度O(nlogn)
  * 最好情况O(nlogn),最差情况O(n^2)(在顺序数列的情况下)
  * @param {Array} arr 待排序的数组
  * @return {Array} 已排序的数组(在原数组上排序)
  */
-function quickSort(arr) {
-  function _qs(leftStart, rightStart) {
+const quickSort = (function() {
+  let _arr = [];
+  const _qs = (leftStart, rightStart) => {
     if (leftStart >= rightStart) {
       return;
     }
     let left = leftStart;
     let right = rightStart;
-    let pivot = arr[leftStart];
+    const pivot = _arr[leftStart];
     while (left < right) {
-      while (arr[right] >= pivot && left < right) {
+      while (_arr[right] > pivot && left < right) {
         right--;
       }
-      while (arr[left] <= pivot && left < right) {
+      while (_arr[left] <= pivot && left < right) {
         left++;
       }
-      [arr[left], arr[right]] = [arr[right], arr[left]]
+      [_arr[left], _arr[right]] = [_arr[right], _arr[left]];
     }
-    arr[leftStart] = arr[left];
-    arr[left] = pivot;
+    _arr[leftStart] = _arr[left];
+    _arr[left] = pivot;
     _qs(leftStart, left - 1);
     _qs(right + 1, rightStart);
-  }
-  _qs(0, arr.length - 1);
-  return arr;
-}
+  };
+  return (arr) => {
+    _arr = arr;
+    _qs(0, arr.length - 1);
+    return arr;
+  };
+})();
 
 /**
- * 快速排序(不在原数组上排序)
+ * 快速排序(不在原数组上排序而是返回新数组)
  * @param {Array} arr 待排序的数组
  * @return {Array} 已排序的数组
  */
