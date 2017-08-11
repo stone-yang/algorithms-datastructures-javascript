8737f2fc8bb3ffffdac3062711d19d1e3f068871
stone-yang <stone-yang@hotmail.com>
2017-08-11 16:59:47 +0800

    edit algorithm permutation

--- algorithms/permutaion.js
+++ algorithms/permutaion.js
@@ -2,13 +2,14 @@
  * 全排列
  * 输出一组无序不重复数字的所有排列方式
  * @param {Array} arr 无序不重复的数组
+ * @param {Number} n 排列位数
  * @return {Number} 存在的排列方式数量
  */
 const permutation = (function() {
 	const stack = [];
 	let count = 0;
-	function _permutation(arr) {
-		if (arr.length === 0) {
+	function _permutation(arr, n) {
+		if (n === 0 || arr.length === 0) {
 			console.log(stack);
 			count++;
 			return;
@@ -17,12 +18,12 @@ const permutation = (function() {
 			stack.push(arr[i]);
 			const newArr = [...arr];
 			newArr.splice(i, 1);
-			_permutation(newArr);
+			_permutation(newArr, n - 1);
 			stack.pop();
 		}
 	}
-	return (arr) => {
-		_permutation(arr);
+	return (arr, n = arr.length) => {
+		_permutation(arr, n);
 		return count;
 	};
 })();
